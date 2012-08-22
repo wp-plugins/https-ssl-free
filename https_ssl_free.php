8b2880c3dcf23ea247db59d09c3ff2e7dd3006ef
mediusproject <mediusproject@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-22 15:12:34 +0000

    git-svn-id: https://plugins.svn.wordpress.org/https-ssl-free/tags/release-1.0.2@588879 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- https_ssl_free.php
+++ https_ssl_free.php
@@ -3,7 +3,7 @@
 Plugin Name: Https SSL Free
 Plugin URI: http://mediusproject.org
 Description: This plugin allows you to log into your wordpress using the free https service from intosecure inc.
-Version: 1.0.1
+Version: 1.0.2
 Author: Medius Project
 Coded by: Leandro D. Pompeo (lp at mediusproject.org)
 Idea by: Andrés Meggiotto (am at mediusproject.org)
